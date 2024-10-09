ca739c23e182ee21ea2f8330e3460af831e98604
Dominika Szymanska <dominika1872@gmail.com>
2024-10-10 00:56:19 +0200

    Add player movement

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -3,5 +3,17 @@ class_name Character
 
 const FRICTION: float = 0.15
 
-export(int) var acceleration: int = 40
-export(int) var max_speed: int = 100
+@export var acceleration: int = 40
+@export var max_speed: int = 100
+
+@onready var animated_sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
+
+var mov_direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+	
+func move() -> void:
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * acceleration
